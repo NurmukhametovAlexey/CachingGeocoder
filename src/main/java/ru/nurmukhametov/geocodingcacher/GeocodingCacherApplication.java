883a9f941a1213a9f88777d17da4e1119b849aca
Alexey Nurmukhametov <alrnurmuhametov@gmail.com>
2021-09-01 12:08:31 +0300

    modified:   src/main/java/ru/nurmukhametov/geocodingcacher/GeocodingCacherApplication.java

--- src/main/java/ru/nurmukhametov/geocodingcacher/GeocodingCacherApplication.java
+++ src/main/java/ru/nurmukhametov/geocodingcacher/GeocodingCacherApplication.java
@@ -8,14 +8,12 @@
     import ru.nurmukhametov.geocodingcacher.exception.DatabaseException;
     import ru.nurmukhametov.geocodingcacher.model.Geocode;
     import ru.nurmukhametov.geocodingcacher.service.CachedGeocodingService;
-    import ru.nurmukhametov.geocodingcacher.service.OuterGeocoder;
 
     @SpringBootApplication
 @Data
 public class GeocodingCacherApplication {
     public static void main(String[] args) throws JsonProcessingException {
         ApplicationContext ctx = SpringApplication.run(GeocodingCacherApplication.class, args);
-
 
         CachedGeocodingService cachedGeocodingService = ctx.getBean(CachedGeocodingService.class);
 
